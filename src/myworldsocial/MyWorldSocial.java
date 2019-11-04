c06360302e13006fb9b41ab4b84e52dc44193594
Gustabatista <>
2019-11-04 09:07:09 -0300

    Segunda parte do projeto MyWorldSocial

---
+++ src/myworldsocial/MyWorldSocial.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myworldsocial;
+
+import views.user.login;
+
+public class MyWorldSocial {
+
+   
+    public static void main(String[] args) {
+        
+    new login().setVisible(true);
+    
+        
+       
+    }
+    
+}
